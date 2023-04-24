d8c6ad6944808c623ffb6951bd6f59a2041c6156
Dariush Wahdany <dariush.wahdany@aisec.fraunhofer.de>
2023-04-24 11:47:16 +0200

    feat: batch memory manager

--- engine_finetune.py
+++ engine_finetune.py
@@ -12,6 +12,7 @@
 import math
 import sys
 from typing import Iterable, Optional
+from contextlib import nullcontext
 
 import torch
 
@@ -33,11 +34,14 @@ def train_one_epoch(
     max_norm: float = 0,
     mixup_fn: Optional[Mixup] = None,
     log_writer=None,
+    args=None,
+    physical_batch_size: Optional[int] = None,
 ):
+
     model.train(True)
     metric_logger = misc.MetricLogger(delimiter="  ")
-    metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    header = 'Epoch: [{}]'.format(epoch)
+    metric_logger.add_meter("lr", misc.SmoothedValue(window_size=1, fmt="{value:.6f}"))
+    header = "Epoch: [{}]".format(epoch)
     print_freq = 20
 
     accum_iter = args.accum_iter
@@ -45,56 +49,73 @@ def train_one_epoch(
     optimizer.zero_grad()
 
     if log_writer is not None:
-        print('log_dir: {}'.format(log_writer.log_dir))
+        print("log_dir: {}".format(log_writer.log_dir))
+    if physical_batch_size:
+        from opacus.utils.batch_memory_manager import BatchMemoryManager
+    with BatchMemoryManager(
+        data_loader=data_loader,
+        max_physical_batch_size=physical_batch_size,
+        optimizer=optimizer,
+    ) if physical_batch_size else nullcontext(data_loader) as new_data_loader:
+        for data_iter_step, (samples, targets) in enumerate(
+            metric_logger.log_every(new_data_loader, print_freq, header)
+        ):
 
-    for data_iter_step, (samples, targets) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+            # we use a per iteration (instead of per epoch) lr scheduler
+            if data_iter_step % accum_iter == 0:
+                lr_sched.adjust_learning_rate(
+                    optimizer, data_iter_step / len(new_data_loader) + epoch, args
+                )
 
-        # we use a per iteration (instead of per epoch) lr scheduler
-        if data_iter_step % accum_iter == 0:
-            lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+            samples = samples.to(device, non_blocking=True)
+            targets = targets.to(device, non_blocking=True)
 
-        samples = samples.to(device, non_blocking=True)
-        targets = targets.to(device, non_blocking=True)
+            if mixup_fn is not None:
+                samples, targets = mixup_fn(samples, targets)
 
-        if mixup_fn is not None:
-            samples, targets = mixup_fn(samples, targets)
+            with torch.cuda.amp.autocast():
+                outputs = model(samples)
+                loss = criterion(outputs, targets)
 
-        with torch.cuda.amp.autocast():
-            outputs = model(samples)
-            loss = criterion(outputs, targets)
+            loss_value = loss.item()
 
-        loss_value = loss.item()
+            if not math.isfinite(loss_value):
+                print("Loss is {}, stopping training".format(loss_value))
+                sys.exit(1)
 
-        if not math.isfinite(loss_value):
-            print("Loss is {}, stopping training".format(loss_value))
-            sys.exit(1)
+            loss /= accum_iter
+            loss_scaler(
+                loss,
+                optimizer,
+                clip_grad=max_norm,
+                parameters=model.parameters(),
+                create_graph=False,
+                update_grad=(data_iter_step + 1) % accum_iter == 0,
+            )
+            if (data_iter_step + 1) % accum_iter == 0:
+                optimizer.zero_grad()
 
-        loss /= accum_iter
-        loss_scaler(loss, optimizer, clip_grad=max_norm,
-                    parameters=model.parameters(), create_graph=False,
-                    update_grad=(data_iter_step + 1) % accum_iter == 0)
-        if (data_iter_step + 1) % accum_iter == 0:
-            optimizer.zero_grad()
+            torch.cuda.synchronize()
 
-        torch.cuda.synchronize()
+            metric_logger.update(loss=loss_value)
+            min_lr = 10.0
+            max_lr = 0.0
+            for group in optimizer.param_groups:
+                min_lr = min(min_lr, group["lr"])
+                max_lr = max(max_lr, group["lr"])
 
-        metric_logger.update(loss=loss_value)
-        min_lr = 10.
-        max_lr = 0.
-        for group in optimizer.param_groups:
-            min_lr = min(min_lr, group["lr"])
-            max_lr = max(max_lr, group["lr"])
+            metric_logger.update(lr=max_lr)
 
-        metric_logger.update(lr=max_lr)
-
-        loss_value_reduce = misc.all_reduce_mean(loss_value)
-        if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
-            """ We use epoch_1000x as the x-axis in tensorboard.
-            This calibrates different curves when batch size changes.
-            """
-            epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
-            log_writer.add_scalar('loss', loss_value_reduce, epoch_1000x)
-            log_writer.add_scalar('lr', max_lr, epoch_1000x)
+            loss_value_reduce = misc.all_reduce_mean(loss_value)
+            if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
+                """We use epoch_1000x as the x-axis in tensorboard.
+                This calibrates different curves when batch size changes.
+                """
+                epoch_1000x = int(
+                    (data_iter_step / len(new_data_loader) + epoch) * 1000
+                )
+                log_writer.add_scalar("loss", loss_value_reduce, epoch_1000x)
+                log_writer.add_scalar("lr", max_lr, epoch_1000x)
 
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
@@ -107,7 +128,7 @@ def evaluate(data_loader, model, device):
     criterion = torch.nn.CrossEntropyLoss()
 
     metric_logger = misc.MetricLogger(delimiter="  ")
-    header = 'Test:'
+    header = "Test:"
 
     # switch to evaluation mode
     model.eval()
